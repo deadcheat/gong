1b68ce3a8a6c8a17adb736ebf6b49b789abae20d
deadcheat <deadcheat@users.noreply.github.com>
2018-06-01 00:54:48 +0900

    added consts

---
+++ values/constants.go
@@ -0,0 +1,17 @@
+package values
+
+// const value of request.type
+const (
+	// TypeLaunchRequest const value of request.type
+	TypeLaunchRequest = "LaunchRequest"
+	// TypeIntentRequest const value of request.type
+	TypeIntentRequest = "IntentRequest"
+	// TypeSessionEndedRequest const value of request.type
+	TypeSessionEndedRequest = "SessionEndedRequest"
+)
+
+// const value of request.intent.name
+const (
+	// IntentGongRing const value of request.intent.name
+	IntentGongRing = "GongRing"
+)
